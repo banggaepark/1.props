eb4be3b38c8f7c89bd86cf0e598c82908efbd2f7
banggaepark <banggae.park@gmail.com>
2021-06-20 14:21:25 +0900

    2차 변경

--- 1.props-exam/src/components/BookList.js
+++ 1.props-exam/src/components/BookList.js
@@ -1,27 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import {List, ListItem} from '@material-ui/core';
-import Books from '../static_data/Books';
+import { List, ListItem, Container } from '@material-ui/core';
 import BookListItem from './BookListItem';
 
-class BookList extends Component{
-    render(){
+
+
+class BookList extends Component {
+    render() {
         const { books } = this.props;
-        return(
-            <List>
+        const bookItems = books.map(book => {
+            return (
                 <ListItem>
-                    <BookListItem book={books[0]} />
+                    <BookListItem book={book} />
                 </ListItem>
-                <ListItem>
-                    { books[1].title }
-                </ListItem>
-                <ListItem>   
-                    Item03
-                </ListItem>
-                <ListItem>
-                    Item04
-                </ListItem>
-            </List>
+            )
+        })
+        return (
+            <Container>
+                <List>
+                    {bookItems}
+                </List>
+            </Container>
         )
     }
 }
